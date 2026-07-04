ac7abca52047cb2ad347816cebbe99e7cbac1cf2
sarwinder - web <manve192930@gmail.com>
2026-07-04 10:53:12 +0530

    FIRST TREE FUNCTION

---
+++ Create_Tree.py
@@ -0,0 +1,13 @@
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+root = TreeNode("10")
+root.left = TreeNode("5")
+root.right = TreeNode("20")
+
+print(root.value)
+print(root.left)
+print(root.right)
